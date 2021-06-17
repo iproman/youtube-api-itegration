6dbfca3eda5ba570f2276c1cc0b86df8c76afdc3
iproman <itechman@yandex.ru>
2021-06-17 16:04:59 +0500

    Add data for search api

--- projects/youtube-api/src/app/youtube-search.service.ts
+++ projects/youtube-api/src/app/youtube-search.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken } from '@angular/core';
+
+const url = 'https://www.googleapis.com/youtube/v3/search';
+const token = 'AIzaSyBfS5Djh48jA2HGns6nGM0IWk8bY5A0IWo';
+
+export const injectUrl = new InjectionToken('url');
+export const injectToken = new InjectionToken('api');
+
+export const apiData = [
+  {
+    provide: injectUrl,
+    useValue: url
+  },
+  {
+    provide: injectToken,
+    useValue: token
+  }
+]
 
 @Injectable({
   providedIn: 'root'
