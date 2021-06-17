3098dfe33e6e20df57e99f15a155a231aaaa350e
iproman <itechman@yandex.ru>
2021-06-17 16:04:48 +0500

    Divide template

--- projects/youtube-api/src/app/views/search-field/search-field.component.ts
+++ projects/youtube-api/src/app/views/search-field/search-field.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-field',
-  template: `
-    <div class="input-group mb-3 mt-5">
-      <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-      <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-    </div>
-  `,
+  templateUrl: './search-field.component.html',
   styles: [
   ]
 })
@@ -18,4 +13,7 @@ export class SearchFieldComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  search(value: string): void {
+    console.log(value)
+  }
 }
