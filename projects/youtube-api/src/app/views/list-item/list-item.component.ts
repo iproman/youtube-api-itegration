fc5651be1b4fb21cb8425b43a74df842e4cc6d0c
iproman <itechman@yandex.ru>
2021-06-18 10:45:29 +0500

    Add card with @input

--- projects/youtube-api/src/app/views/list-item/list-item.component.ts
+++ projects/youtube-api/src/app/views/list-item/list-item.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Video } from '../../Video';
 
 @Component({
   selector: 'app-list-item',
   template: `
-    <p>
-      list-item works!
-    </p>
+    <div class="card">
+      <img class="card-img-top" [src]="video.imageUrl" alt="Card image cap">
+      <div class="card-body">
+        <a class="card-title" href="#" >{{video.title}}</a>
+        <p class="card-text">{{video.description}}</p>
+      </div>
+    </div>
   `,
   styles: [
   ]
 })
 export class ListItemComponent implements OnInit {
+
+  // @ts-ignore
+  @Input() video: Video;
 
   constructor() { }
 
